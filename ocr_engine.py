f0d1d2afe06db839db4794017086608835a68ae4
bomblin <k_stachowicz@outlook.com>
2020-11-14 06:22:33 +0100

    added pdf converter

--- ocr_engine.py
+++ ocr_engine.py
@@ -1,13 +1,13 @@
 import pytesseract
 from PIL import Image
-import argparse
+import argpars
 import cv2
 import os
 import pdf2image
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
     help="path to input image to be OCR'd")
-ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+ap.add_argument("-p", "--preprocess", type=str, default=None,
     help="type of preprocessing to be done")
 args = vars(ap.parse_args())
 
@@ -16,21 +16,23 @@ image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # check to see if we should apply thresholding to preprocess the
 # image
-if args["preprocess"] == "thresh":
-    gray = cv2.threshold(gray, 0, 255,
-        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+#if args["preprocess"] == "thresh":
+ #   gray = cv2.threshold(gray, 0, 255,
+#        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 # make a check to see if median blurring should be done to remove
 # noise
-elif args["preprocess"] == "blur":
-    gray = cv2.medianBlur(gray, 3)
+#elif args["preprocess"] == "blur":
+#    gray = cv2.medianBlur(gray, 3)
 # write the grayscale image to disk as a temporary file so we can
 # apply OCR to it
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
+#filename = "{}.png".format(os.getpid())
+#cv2.imwrite(filename, gray)
 text = pytesseract.image_to_string(Image.open(filename), lang='pol')
 os.remove(filename)
 print(text)
+
 # show the output images
 cv2.imshow("Image", image)
 cv2.imshow("Output", gray)
-#cv2.waitKey(0)
+cv2.waitKey(0)
+
---
+++ ocrv2.py
@@ -0,0 +1,36 @@
+import pytesseract as ocr
+from PIL import Image
+import fitz
+
+class Reader():
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read(self, file_name):
+        self.text = ocr.image_to_string(Image.open(file_name), lang = 'pol')
+        print(self.text)
+        return self.text
+
+    def load_and_convert(self):
+        pdf = self.filename
+        temp = fitz.open(pdf)
+        for p in range(10):
+            try:
+                page = temp.loadPage(p)
+            except:
+                zoom_factor = 2.5
+                zoom_matrix = fitz.Matrix(zoom_factor, zoom_factor)
+                output = page.getPixmap(matrix = zoom_matrix)
+                out = f"{self.filename}.png"
+                img = output.writePNG(out)
+        return out
+        
+
+
+
+reader = Reader('test1.pdf')
+
+reader.read(reader.load_and_convert())
+
+
+
